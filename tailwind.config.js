827958826ea395caa9b62fecb46a4f837114e850
Paradiess-svg <izzan4izdihar@gmail.com>
2023-01-25 15:54:47 +0700

    tambahkan warna dan intro walau intro blom selesai

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        warna1: 'rgba(52, 152, 219, 1)',
+        warna2: 'rgba(41, 128, 185, 1)',
+        warna3: 'rgba(52, 73, 94, 1)',
+        warna4: 'rgba(241, 196, 15, 1)',
+        warna5: 'rgba(243, 156, 18, 1)',
+        warna6: 'rgba(44, 52, 135, 1)'
+      },
+    },
   },
   plugins: [],
 }
