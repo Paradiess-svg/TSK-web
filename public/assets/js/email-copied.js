6974d5315ca0dd04d79c67a4be559492ce0389bb
Paradiess-svg <izzan4izdihar@gmail.com>
2023-02-06 13:01:46 +0700

    menambahkan nomor telephone dan email

--- public/assets/js/email-copied.js
+++ public/assets/js/email-copied.js
@@ -1,6 +1,6 @@
 var btn = document.querySelector("#btn");
 
 btn.addEventListener("click", () => {
-  const textCopied = ClipboardJS.copy("tetambastudio@gmail.com");
+  const textCopied = ClipboardJS.copy("Contact@tetambastudio.com");
   element.classList.add("copied");
 });
