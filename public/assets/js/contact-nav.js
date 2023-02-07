81bf3130d3889dc3a0fc9e8903ab7568b17bda33
Verrellaksono <101404993+Verrellaksono@users.noreply.github.com>
2023-02-07 15:52:06 +0700

    membenarkan warna nav link

--- public/assets/js/contact-nav.js
+++ public/assets/js/contact-nav.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-      // sticky navbar on scroll script
-      if (this.scrollY > 20) {
-        $('.contact-nav').addClass("text-warna3");
-      } else {
-        $('.contact-nav').removeClass("text-warna3");
-      }
-    });
-
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".contact-nav").addClass("text-warna6");
+      $(".contact-nav").removeClass("text-warna5");
+    } else {
+      $(".contact-nav").removeClass("text-warna6");
+      $(".contact-nav").addClass("text-warna5");
+    }
   });
+});
