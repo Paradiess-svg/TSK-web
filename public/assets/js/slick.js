493375c1811c6fbc10e7429248ab01244d65e842
Paradiess-svg <izzan4izdihar@gmail.com>
2023-02-15 14:49:37 +0700

    team card v1

--- public/assets/js/slick.js
+++ public/assets/js/slick.js
@@ -13,23 +13,5 @@ $(document).ready(function () {
 
 
 
-  $('.fade').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    fade: true,
-    autoplaySpeed: 2000,
-    dots: true,
-    arrows:true  
-  });
 
-  $('.slide-prev').click(function(e){ 
-    //e.preventDefault(); 
-$('.fade').slick('slickPrev');
-} );
-
-$('.slide-next').click(function(e){
-//e.preventDefault(); 
-$('.fade').slick('slickNext');
-} );  
 });
