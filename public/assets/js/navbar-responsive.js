81bca0e9746946aed1a651a8b430e4091a22605a
Verrellaksono <101404993+Verrellaksono@users.noreply.github.com>
2023-02-22 11:57:22 +0700

    fix

--- public/assets/js/navbar-responsive.js
+++ public/assets/js/navbar-responsive.js
@@ -1,23 +1,28 @@
 function Menu(e) {
-    let list = document.querySelector('ul');
-    e.name === 'menu' ? (e.name = "close", list.classList.add('top-[80px]'), list.classList.add('opacity-100'), list.classList.add('z-[-10]')) : (e.name = "menu", list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'))
-  };
+  let list = document.querySelector("ul");
+  e.name === "menu"
+    ? ((e.name = "close"),
+      list.classList.add("top-[80px]"),
+      list.classList.add("opacity-100"),
+      list.classList.add("z-[-10]"))
+    : ((e.name = "menu"),
+      list.classList.remove("top-[80px]"),
+      list.classList.remove("opacity-100"));
+}
 
-  $(document).ready(function () {
-    $(".menu-click").click(function () {
-      $(".navbar").toggleClass("sticky-costum-2");
-    });
+$(document).ready(function () {
+  $(".menu-click").click(function () {
+    $(".navbar").toggleClass("sticky-costum-2");
   });
+});
 
-  $(document).ready(function () {
-    $(window).scroll(function () {
-      // sticky navbar on scroll script
-      if (this.scrollY > 20) {
-        $('.navbar').addClass("sticky-costum");
-      } else {
-        $('.navbar').removeClass("sticky-costum");
-      }
-    });
-
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky-costum");
+    } else {
+      $(".navbar").removeClass("sticky-costum");
+    }
   });
-
+});
